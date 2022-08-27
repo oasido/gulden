69a9f50ce1ca63556dc649f39ec8ef6812a21295
Ofek Asido <asido.ofek@gmail.com>
2022-08-27 13:29:02 +0300

    style: increase container size

--- components/PageLayout/index.tsx
+++ components/PageLayout/index.tsx
@@ -10,7 +10,7 @@ export const PageLayout: FunctionComponent<Props> = ({ children }) => {
   return (
     <>
       <Navbar />
-      <Container>{children}</Container>
+      <Container size="xl">{children}</Container>
     </>
   );
 };
