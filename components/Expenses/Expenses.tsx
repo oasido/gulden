7e36ccfbf153c18f903045024a5f5fc9eb1ffb42
oasido <asido.ofek@gmail.com>
2022-08-29 20:00:10 +0300

    feat: add expense searching functionality

--- components/Expenses/Expenses.tsx
+++ components/Expenses/Expenses.tsx
@@ -1,4 +1,4 @@
-import { createStyles, Title, Table } from '@mantine/core';
+import { ChangeEvent, useState } from 'react';
 import { Expense } from '../../types/generic';
 
 const useStyles = createStyles((theme) => ({
@@ -10,6 +10,14 @@ const useStyles = createStyles((theme) => ({
 
 export const Expenses = (expenses: Expense[]) => {
   const { classes } = useStyles();
+  const [expenseData, setExpenseData] = useState<Expense[]>([
+    { id: 6, price: 12.011, date: '20/11/22', name: 'Carbon' },
+    { id: 7, price: 14.007, date: '20/11/22', name: 'Nitrogen' },
+    { id: 39, price: 88.906, date: '20/11/22', name: 'Yttrium' },
+    { id: 56, price: 137.33, date: '20/11/22', name: 'Barium' },
+    { id: 58, price: 140.12, date: '20/11/22', name: 'Cerium' },
+  ]);
+  const [searchInput, setSearchInput] = useState<string>('');
 
   const elements = [
     { position: 6, price: 12.011, date: '20/11/2022', name: 'Carbon' },
@@ -19,20 +27,26 @@ export const Expenses = (expenses: Expense[]) => {
     { position: 58, price: 140.12, date: '20/11/2022', name: 'Cerium' },
   ];
 
-  const rows = elements.map((e, idx) => (
-    <tr key={idx}>
-      <td>{e.position}</td>
-      <td>{e.name}</td>
-      <td>{e.price}</td>
-      <td>{e.date}</td>
-    </tr>
-  ));
+  // implement with useMemo
+  const handleSearch = (string: string) => {
+    setSearchInput(string);
+    const filtered = elements.filter((expense: Expense) =>
+      expense.name.toLowerCase().includes(string.toLowerCase())
+    );
+    console.log(filtered);
+    setExpenseData(filtered);
+  };
 
   return (
     <div>
       <Title order={4} className={classes.title}>
         Expenses
       </Title>
+      <Input
+        placeholder="Search..."
+        value={searchInput}
+        onChange={(evt: ChangeEvent<HTMLInputElement>) => handleSearch(evt.target.value)}
+      />
       <Table highlightOnHover verticalSpacing="sm">
         <thead>
           <tr>
