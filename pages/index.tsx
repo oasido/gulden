280c195eec9f53d78a10baaa54750c957920bf8b
Ofek Asido <asido.ofek@gmail.com>
2022-09-03 23:40:01 +0300

    chore: (wip) pass down expense data from db into the expense component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,32 @@
 import type { NextPage } from 'next';
 import Expenses from '../components/Expenses';
 import { PageLayout } from '../components/PageLayout';
+import { InferGetServerSidePropsType } from 'next';
+import dbConnect from '../lib/dbConnect';
+import Expense from '../models/Expense';
 
-const Home: NextPage = () => {
+export const getServerSideProps = async () => {
+  try {
+    await dbConnect();
+    const data = await Expense.find({});
+
+    return {
+      props: { expenses: JSON.stringify(data) },
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      props: {},
+    };
+  }
+};
+
+const Home: NextPage = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const { expenses } = props;
+
   return (
     <PageLayout>
-      <Expenses />
+      <Expenses expenses={JSON.parse(expenses)} />
     </PageLayout>
   );
 };
