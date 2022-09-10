48dbac5b4bd2c7989609d31cd5607b0638298bfe
Ofek Asido <asido.ofek@gmail.com>
2022-09-10 23:06:35 +0300

    feat: make search work

--- components/Expenses/Expenses.tsx
+++ components/Expenses/Expenses.tsx
@@ -1,6 +1,6 @@
 import { ChangeEvent, useState } from 'react';
-import { createStyles, Title, Table, Input, Group } from '@mantine/core';
-import { Expense } from '../../types/generic';
+import { createStyles, Title, Table, Input, Group, Container } from '@mantine/core';
+import { Expense } from 'types/generic';
 import { AddExpenseModal } from './AddExpenseModal';
 
 const useStyles = createStyles(() => ({
@@ -11,60 +11,63 @@ const useStyles = createStyles(() => ({
 }));
 
 export const Expenses = ({ expenses }: { expenses: string }) => {
-  const { classes } = useStyles();
+  const { classes, theme } = useStyles();
 
-  const [expenseData, setExpenseData] = useState(JSON.parse(expenses));
+  const [expensesArr, setExpensesArr] = useState(JSON.parse(expenses));
+  const [filtered, setFiltered] = useState(expensesArr);
   const [searchInput, setSearchInput] = useState<string>('');
 
   // implement with useMemo
   const handleSearch = (string: string) => {
     setSearchInput(string);
-    const filtered = [...expenseData].filter((expense: Expense) =>
+    const search = expensesArr.filter((expense: Expense) =>
       expense.name.toLowerCase().includes(string.toLowerCase())
     );
-    console.log(filtered);
-    setExpenseData(filtered);
+    setFiltered(search);
   };
 
   return (
     <div>
-      <Group position="apart">
-        <Title order={4} className={classes.title}>
-          Expenses
-        </Title>
+      <Container>
+        <Group position="apart">
+          <Title order={4} className={classes.title}>
+            Expenses
+          </Title>
           <AddExpenseModal setFiltered={setFiltered} />
-      </Group>
-      <Input
-        placeholder="Search..."
-        value={searchInput}
-        onChange={(evt: ChangeEvent<HTMLInputElement>) => handleSearch(evt.target.value)}
-      />
-      <Table highlightOnHover verticalSpacing="sm">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Name</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          {expenseData.map((e: Expense, idx: number) => (
-            <tr key={idx}>
-              <td>{e.date}</td>
-              <td>{e.name}</td>
-              <td>{e.price}</td>
-            </tr>
-          ))}
-          {expenseData.length === 0 && (
+        </Group>
+        <Input
+          placeholder="Search..."
+          value={searchInput}
+          onChange={(evt: ChangeEvent<HTMLInputElement>) => handleSearch(evt.target.value)}
+        />
+        <Table highlightOnHover fontSize="md" verticalSpacing="sm" mb={100}>
+          <thead>
             <tr>
-              <td></td>
-              <td>No results found.</td>
-              <td></td>
+              <th></th>
+              <th>Date</th>
+              <th>Name</th>
+              <th>Price</th>
             </tr>
-          )}
-        </tbody>
-      </Table>
-      <>{expenses}</>
+          </thead>
+          <tbody>
+            {filtered.reverse().map((e: Expense, idx: number) => (
+              <tr key={idx}>
+                <td>X</td>
+                <td>{e.date}</td>
+                <td>{e.name}</td>
+                <td>{e.price}</td>
+              </tr>
+            ))}
+            {filtered.length === 0 && (
+              <tr>
+                <td></td>
+                <td>No results found.</td>
+                <td></td>
+              </tr>
+            )}
+          </tbody>
+        </Table>
+      </Container>
     </div>
   );
 };
