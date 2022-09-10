7745a1829fc5c93d381c6d4f9c30c903dbb1f605
Ofek Asido <asido.ofek@gmail.com>
2022-09-10 23:08:07 +0300

    feat: implement endpoint to add expense, verify that the data is valid and write to db

--- pages/api/expense/add.ts
+++ pages/api/expense/add.ts
@@ -1,28 +1,34 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { Expense } from '../../../types/generic';
+import type { Expense } from 'types/generic';
+import { z } from 'zod';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import clientPromise from '@lib/mongodb';
 
-type Data = {
-  name: string;
-};
+const expenseSchema = z.object({
+  name: z.string().trim().min(2),
+  date: z.string().trim().min(1, { message: 'Invalid date' }),
+  price: z.number(),
+});
 
-type Request = {};
-
-const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
-    try {
-      const client = await clientPromise;
-      const db = client.db('gulden');
-      const collection = db.collection('expenses');
-      const foundExpenses = await collection.find({}).toArray();
+    const { date, name, price }: Expense = req.body;
+    console.log(req.body);
+    console.log(date, name, price);
+    const parse = expenseSchema.safeParse({ date, name, price });
 
-      res.status(200).json(foundExpenses);
-    } catch (err) {
-      console.error(err);
+    if (parse.success === false) {
+      res.status(400).json(parse.error.errors);
+      return;
     }
+
+    const client = await clientPromise;
+    const db = client.db('gulden');
+    const collection = db.collection('expenses');
+    const response = await collection.insertOne({ date, name, price });
+    res.status(200).json(response);
   } else {
-    res.status(400).json({ msg: 'Error code 400, bad request method.' });
+    res.status(400).json('Error code 400, bad request method.');
   }
 };
 
