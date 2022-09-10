ce3acc477d82d868c52f7e41360cdb9ebd72a1fe
Ofek Asido <asido.ofek@gmail.com>
2022-09-10 23:07:20 +0300

    refactor: change expense variable name

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,32 +4,37 @@ import { PageLayout } from '@components/PageLayout';
 import clientPromise from '@lib/mongodb';
 import { Text } from '@mantine/core';
 import { useSession } from 'next-auth/react';
+import { useStore } from '@context/useStore';
 
 export const getServerSideProps = async () => {
   try {
     const client = await clientPromise;
     const db = client.db('gulden');
-    const expenses = await db.collection('expenses').find({}).toArray();
+    const expenseData = await db.collection('expenses').find({}).toArray();
 
     return {
-      props: { expenses: JSON.stringify(expenses) },
+      props: { expenseData: JSON.stringify(expenseData) },
     };
   } catch (e) {
     console.error(e);
     return {
       props: {
-        expenses: '[]',
+        expenseData: '[]',
       },
     };
   }
 };
 
-const Home: NextPage<{ expenses: string }> = ({ expenses }) => {
+const Home: NextPage<{ expenseData: string }> = ({ expenseData }) => {
   const { data: session } = useSession();
 
   return (
     <PageLayout>
-      {session ? <Expenses expenses={expenses} /> : <Text mt="lg">You need to log in first!</Text>}
+      {session ? (
+        <Expenses expenses={expenseData} />
+      ) : (
+        <Text mt="lg">You need to log in first!</Text>
+      )}
     </PageLayout>
   );
 };
