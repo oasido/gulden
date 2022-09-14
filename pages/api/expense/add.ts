82a05c913857e3d69da918138fb865a981c30d0e
Ofek Asido <asido.ofek@gmail.com>
2022-09-15 00:17:37 +0300

    feat: add user email to expense additions

--- pages/api/expense/add.ts
+++ pages/api/expense/add.ts
@@ -5,6 +5,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import clientPromise from '@lib/mongodb';
 
 const expenseSchema = z.object({
+  user: z.string().trim().email(),
   name: z.string().trim().min(2),
   date: z.string().trim().min(1, { message: 'Invalid date' }),
   price: z.number(),
@@ -12,10 +13,8 @@ const expenseSchema = z.object({
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
-    const { date, name, price }: Expense = req.body;
-    console.log(req.body);
-    console.log(date, name, price);
-    const parse = expenseSchema.safeParse({ date, name, price });
+    const { user, date, name, price }: Expense = req.body;
+    const parse = expenseSchema.safeParse({ user, date, name, price });
 
     if (parse.success === false) {
       res.status(400).json(parse.error.errors);
@@ -25,7 +24,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const client = await clientPromise;
     const db = client.db('gulden');
     const collection = db.collection('expenses');
-    const response = await collection.insertOne({ date, name, price });
+    const response = await collection.insertOne({ user, date, name, price });
     res.status(200).json(response);
   } else {
     res.status(400).json('Error code 400, bad request method.');
