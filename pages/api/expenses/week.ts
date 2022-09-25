867077e0e5bb8d3252b6759301c9e7b4dbd768c6
Ofek Asido <asido.ofek@gmail.com>
2022-09-25 18:34:17 +0300

    chore: work on past week data retrieval

--- pages/api/expenses/week.ts
+++ pages/api/expenses/week.ts
@@ -1,44 +1,91 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { Expense } from 'types/generic';
+import type { Expense, ExpenseSearchQueryResult } from 'types/generic';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import clientPromise from '@lib/mongodb';
 import { unstable_getServerSession } from 'next-auth/next';
 import { authOptions } from '../auth/[...nextauth]';
-import dayjs from 'dayjs';
+import { eachDayOfInterval, format, isSameDay, subDays } from 'date-fns';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const session = await unstable_getServerSession(req, res, authOptions);
 
-  // if (session) {
-  if (req.method === 'GET') {
-    const { user } = req.body;
+  if (session) {
+    if (req.method === 'GET') {
+      const { user } = req.body;
+      // TODO: Show only user specific data
 
-    const client = await clientPromise;
-    console.log(dayjs().subtract(1, 'week').toISOString());
-    const db = client.db('gulden');
-    const collection = db.collection('expenses');
-    // user,
-    const response = await collection
-      .aggregate([
-        {
-          $match: {
-            date: {
-              $gte: new Date(dayjs().subtract(1, 'week').toISOString()),
-              $lte: new Date(),
+      const client = await clientPromise;
+      const db = client.db('gulden');
+      const collection = db.collection('expenses');
+
+      const date = new Date(new Date().setHours(0, 0, 0, 0));
+      const weekPastNow = subDays(date, 7);
+
+      const dbQueryResult = await collection
+        .aggregate([
+          {
+            $match: {
+              date: {
+                $gte: weekPastNow,
+                $lte: date,
+              },
             },
           },
-        },
-        { $group: { _id: { $dateToString: { date: '$date' } }, spent: { $sum: '$price' } } },
-      ])
-      .toArray();
-    res.status(200).json(response);
+          {
+            $group: {
+              _id: '$date',
+              spent: { $sum: '$price' },
+            },
+          },
+        ])
+        .toArray();
+
+      const getLastWeekArray = () => {
+        const date = new Date(new Date().setHours(0, 0, 0, 0));
+        const weekPastNow = subDays(date, 7);
+
+        const weekArray = eachDayOfInterval({
+          start: weekPastNow,
+          end: date,
+        }).map((day) => ({
+          label: format(day, 'dd/MM'),
+          date: day,
+        }));
+
+        return weekArray;
+      };
+
+      const responseData = getLastWeekArray().map((day) => {
+        const { date } = day;
+
+        const parsedObj = dbQueryResult.find((o: ExpenseSearchQueryResult, idx: number) => {
+          console.log(o._id);
+          const isFound = isSameDay(o._id, date);
+          if (isFound) {
+            dbQueryResult.splice(idx, 1);
+            return isFound;
+          }
+        });
+
+        if (parsedObj === undefined) {
+          return { ...day, spent: 0 };
+        } else {
+          return { ...day, date: parsedObj._id, spent: parsedObj.spent };
+        }
+      });
+
+      // const getExpensesDataArr: number[] = responseData.map((day) => day.spent);
+
+      // const getExpensesLabelsArr: string[] = responseData.map((day) => day.label);
+
+      res.status(200).json(responseData);
+    } else {
+      res.status(400).json('Error code 400, bad request method.');
+    }
   } else {
-    res.status(400).json('Error code 400, bad request method.');
+    // Not Signed in
+    res.status(401).json('Unauthorized request.');
   }
-  // } else {
-  //   // Not Signed in
-  //   res.status(401).json('Unauthorized request.');
-  // }
 };
 
 export default handler;
