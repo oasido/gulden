33eb40e1f77f50bfcd4ac7f9220444a086875420
Ofek Asido <asido.ofek@gmail.com>
2022-10-04 16:57:36 +0300

    chore: update navbar

--- components/PageLayout/Navbar.tsx
+++ components/PageLayout/Navbar.tsx
@@ -3,6 +3,7 @@ import { signIn, signOut, useSession } from 'next-auth/react';
 import { FcGoogle } from 'react-icons/fc';
 import { BiLogOutCircle } from 'react-icons/bi';
 import { NextLink } from '@mantine/next';
+import { FaUser } from 'react-icons/fa';
 
 const useStyles = createStyles((theme) => ({
   navbarWrapper: {
@@ -33,53 +34,71 @@ export const Navbar = () => {
   const { data: session } = useSession();
 
   return (
-    <div className={classes.navbar}>
+    <div className={classes.navbarWrapper}>
+      <div className={classes.navbar}>
+        <Title order={1} className={classes.title}>
+          💰 Gulden
+        </Title>
+        {!session ? (
+          <Button
+            leftIcon={<FcGoogle />}
+            color="gray"
+            variant="subtle"
             onClick={() => signIn('google')}
             size="lg"
             compact
             radius="xl"
           >
-          Login with Google
-        </Button>
-      ) : (
-        <Menu
-          trigger="hover"
-          closeDelay={500}
-          withArrow
-          shadow="lg"
-          transition="scale-y"
-          transitionDuration={150}
-        >
-          <Menu.Target>
+            Login with Google
+          </Button>
+        ) : (
+          <Menu
+            closeDelay={500}
+            shadow="lg"
+            transition="scale-y"
+            transitionDuration={150}
+            position="bottom-end"
+          >
+            <Menu.Target>
+              <Button
                 compact
                 radius="xl"
                 size="lg"
+                // mr={20}
+                variant="outline"
+                color="gray"
+                leftIcon={<FaUser />}
+                className={classes.button}
+              >
+                {session.user?.name}
+              </Button>
+            </Menu.Target>
 
-          <Menu.Dropdown>
-            <Text size="xs" color="dimmed" my={5} mx={5}>
-              {session.user?.email}
-            </Text>
-            <Menu.Label>Application</Menu.Label>
-            <Menu.Item component={NextLink} href="/settings">
-              Settings
-            </Menu.Item>
-            <Menu.Item
-              rightSection={
-                <Text size="xs" color="dimmed">
-                  ⌘K
-                </Text>
-              }
-            >
-              Search
-            </Menu.Item>
-            <Menu.Divider />
-            <Menu.Item>About</Menu.Item>
-            <Menu.Item onClick={() => signOut()} icon={<BiLogOutCircle />} color="red">
-              Logout
-            </Menu.Item>
-          </Menu.Dropdown>
-        </Menu>
-      )}
+            <Menu.Dropdown mr={20}>
+              <Text size="xs" color="dimmed" my={5} mx={5}>
+                {session.user?.email}
+              </Text>
+              <Menu.Item component={NextLink} href="/settings">
+                Settings
+              </Menu.Item>
+              <Menu.Item
+                rightSection={
+                  <Text size="xs" color="dimmed">
+                    ⌘K
+                  </Text>
+                }
+              >
+                Search
+              </Menu.Item>
+              <Menu.Divider />
+              <Menu.Item>About</Menu.Item>
+              <Menu.Item onClick={() => signOut()} icon={<BiLogOutCircle />} color="red">
+                Logout
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+        )}
+      </div>
     </div>
   );
 };
